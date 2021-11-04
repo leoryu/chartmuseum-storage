9da39e8f51706784ed93b995d955c5a7ae1e8154
leonarliu <leonarliu@tencent.com>
2021-11-04 11:27:34 +0800

    fix: update version of etcd and grpc

--- etcd.go
+++ etcd.go
@@ -9,8 +9,8 @@ import (
 	"sync"
 	"time"
 
-	"go.etcd.io/etcd/clientv3"
-	"go.etcd.io/etcd/pkg/transport"
+	"go.etcd.io/etcd/client/pkg/v3/transport"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 const DefaultPrefix = "/chart_backend_bucket"
